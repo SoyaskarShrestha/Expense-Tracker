e3d97e54eb9f7485a77839d19894f2b4455975f4
Soyaskar <soyaskarshrestha@gmail.com>
2026-05-20 21:46:35 +0545

    feat: add manual chunking configuration for optimized build in Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],// Source - https://stackoverflow.com/a/75719691
+// Posted by Zahid Hassan Shaikot, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-05-20, License - CC BY-SA 4.0
+
+build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
+
 })
